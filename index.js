0125fab864905ec8ae8da13f4857bc1ded022fb4
Syed Muhemin Ali <syedalihussain100>
2020-12-20 03:57:34 -0800

    changes

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+const members = [
+  {
+    id: 1,
+    name: "Zaid",
+    email: "zaid.@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Hammad",
+    email: "hammad@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Muhemin",
+    email: "muhemin@gmail.com",
+  },
+];
+
+app.get("/api/members", (req,res) => {
+  res.json(members);
 });
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3001;
 
